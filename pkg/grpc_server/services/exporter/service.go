16d02ee54b7174b32293262ff7691eed36582ad4
Fred Chien <cfsghost@gmail.com>
2020-10-10 21:07:18 +0800

    improve performance

--- pkg/grpc_server/services/exporter/service.go
+++ pkg/grpc_server/services/exporter/service.go
@@ -1,11 +1,19 @@
 package exporter
 
 import (
+	"sync/atomic"
+
 	"golang.org/x/net/context"
 
 	pb "github.com/BrobridgeOrg/gravity-api/service/exporter"
 	app "github.com/BrobridgeOrg/gravity-exporter-nats/pkg/app"
+	"github.com/prometheus/common/log"
 )
+
+var counter uint64 = 0
+var SendEventSuccess = pb.SendEventReply{
+	Success: true,
+}
 
 type Service struct {
 	app app.App
@@ -16,11 +24,16 @@ func NewService(a app.App) *Service {
 	service := &Service{
 		app: a,
 	}
-
 	return service
 }
 
 func (service *Service) SendEvent(ctx context.Context, in *pb.SendEventRequest) (*pb.SendEventReply, error) {
+
+	id := atomic.AddUint64((*uint64)(&counter), 1)
+
+	if id%1000 == 0 {
+		log.Info(id)
+	}
 
 	conn := service.app.GetEventBus().GetConnection()
 
@@ -31,7 +44,5 @@ func (service *Service) SendEvent(ctx context.Context, in *pb.SendEventRequest) 
 		}, err
 	}
 
-	return &pb.SendEventReply{
-		Success: true,
-	}, nil
+	return &SendEventSuccess, nil
 }
